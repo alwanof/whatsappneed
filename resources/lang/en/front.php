f6cb60c6d2a3035ee468e1fe085296af6b2d3424
Murad Alwan <free1soft@gmail.com>
2021-04-22 00:13:56 +0300

    front index reeady att1

--- resources/lang/en/front.php
+++ resources/lang/en/front.php
@@ -13,57 +13,55 @@ return [
 
     ],
     'HERO' => [
-        'TITLE' => 'Launch Your SaaS Website in Minutes',
-        'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua',
+        'TITLE' => 'Share your Products or Services Through WhatsApp ',
+        'DESC' => 'Upload & share your products or services, connect with your customer through WhatsApp, understand customers needs & communicate with them easier & faster',
+
         'BTN' => 'Get Started'
     ],
     'FEATURES' => [
         0 => [
-            'TITLE' => 'Bootstrap 5',
-            'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore'
+            'TITLE' => 'Secure',
+            'DESC' => 'Ensure the conversations between your business and customers remain private with end-to-end encryption and multi-factor authentication'
         ],
         1 => [
-            'TITLE' => 'Clean Design',
-            'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore'
+            'TITLE' => 'Automated',
+            'DESC' => 'You can get the orders message, delivery link, track orders link & location, price and all this with one click'
         ],
         2 => [
-            'TITLE' => 'Easy to Use',
-            'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore'
+            'TITLE' => 'Easy to use',
+            'DESC' => 'Our dashboard is simple and easy to use. Once you have registered your informations it is stored in the algorithms of our site'
         ],
     ],
     'HOW1' => [
         'TITLE' => 'Perfect Solution Thriving Online Business',
-        'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua',
+        'DESC' => 'We are the only platform that provides professional online stores for small or large businesses and connect you with WhatsApp directly so you and customers can get in touch before purchasing the order, to create credibility and reliability for your business',
     ],
     'HOW2' => [
         'TITLE' => 'Easy to Use with Tons of Awesome Features',
-        'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua',
-    ],
-    'HOW3' => [
-        'TITLE' => 'What our customers says',
-        'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua',
+        'DESC' => 'Our service has many advantages that facilitate the process of communication between you and your customers via WhatsApp. One of the most prominent features is the ability to add your delivery driver in the same control panel to connect more with your customers and give them the possibility to track the order.',
+
     ],
     'WHY' => [
-        'TITLE' => 'Why Choose SaaSpal',
-        'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore',
+        'TITLE' => 'Why Choose whatsappneed',
+        'DESC' => 'With the Covid-19 Pandemic, some habits have changed. Whatsappneed provides you a new mode of life in the context of this health crisis.',
         'ITEMS' => [
             0 => [
-                'TITLE' => 'Why Choose SaaSpal',
-                'DESC' => 'Lorem ipsum dolor sit amet'
+                'TITLE' => 'QR Creator',
+                'DESC' => 'You can create QR Code for your page links, delivery, and whatsapp No. from your dashboard to share your E-store via whatsapp or other social media ads easily'
             ],
             1 => [
-                'TITLE' => 'Why Choose SaaSpal',
-                'DESC' => 'Lorem ipsum dolor sit amet'
+                'TITLE' => 'Online Store',
+                'DESC' => 'Create your online store with your name, brand name, logo, banner for your page and unlimited uploads, adjust your description and prices, no rules or conditions in your e-store you can add privacy and terms in your page and many  other features'
             ],
             2 => [
-                'TITLE' => 'Why Choose SaaSpal',
-                'DESC' => 'Lorem ipsum dolor sit amet'
+                'TITLE' => 'Ready to Use',
+                'DESC' => 'After registering your business in our platform you will be able to create your digital menu to stand out and earn more engagement with your customers.'
             ]
         ]
     ],
     'PRICING' => [
-        'TITLE' => 'Why Choose SaaSpal',
-        'DESC' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore',
+        'TITLE' => 'Choose a Plan',
+        'DESC' => 'Choose one of our plans and make your business interactive',
         'BTN' => 'Get Start',
         'ITEMS' => [
             0 => [
@@ -115,7 +113,7 @@ return [
         'DESC' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id nobis voluptas inventore?',
         'NAME' => 'Name',
         'PHONE' => 'Phone',
-        'COMPANY' => 'Company',
+        'COMPANY' => 'whatsappneed',
         'SEND' => 'Send',
     ]
 
